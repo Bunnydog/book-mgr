b8f9bc2056f806cfae4f9b1003419abf16efc5f4
VincentsPC <xishenging@foxmail.com>
2021-11-25 17:03:16 +0800

    完成大致的登录界面

---
+++ book-mgr-fe/src/views/Auth/index.js
@@ -0,0 +1,14 @@
+
+import { defineComponent } from "vue";
+import {UserOutlined} from "@ant-design/icons-vue"
+
+
+export default defineComponent({
+     components:{
+          UserOutlined,
+     },
+
+     setup(){
+
+     },
+});
---
+++ book-mgr-fe/src/views/Auth/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="auth">
+    <div class="bg-img"></div>
+    <div class="title">
+      <img src="../../assets/img/title-logo.png" alt="" />
+      <h2>图书管理系统</h2>
+    </div>
+
+    <div class="form">
+      <a-tabs default-active-key="1">
+        <a-tab-pane key="1" tab="登录">
+          <div class="item">
+            <a-input size="large" placeholder="账号">
+              <a-icon slot="prefix" type="user" >
+              123</a-icon>
+            </a-input>
+          </div>
+          <div class="item">
+            <a-input size="large" type="password" placeholder="密码" />
+          </div>
+          <div class="item">
+            <a href="javascript:;">忘记密码</a>
+          </div>
+
+          <div class="item">
+            <a-button size="large" type="primary"> 登入 </a-button>
+          </div>
+        </a-tab-pane>
+        <a-tab-pane key="2" tab="注册">
+          <div class="item">
+            <a-input size="large" placeholder="账号" />
+          </div>
+          <div class="item">
+            <a-input size="large" type="password" placeholder="密码" />
+          </div>
+          <div class="item">
+            <a-input size="large" placeholder="邀请码" />
+          </div>
+          <div class="item">
+            <a-button size="large" type="primary"> 注册 </a-button>
+          </div>
+        </a-tab-pane>
+      </a-tabs>
+    </div>
+  </div>
+</template>
+
+<script src = './index.js'>
+</script>
+
+
+<style lang="scss" scoped>
+@import "./index.scss";
+</style>
